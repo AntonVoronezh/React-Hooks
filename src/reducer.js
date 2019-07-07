094d8c459f3392666d03c5634a20505039cc3eb6
AntonVoronezh <pstut@mail.ru>
2019-07-07 17:20:02 +0300

    finish

--- src/reducer.js
+++ src/reducer.js
@@ -9,6 +9,16 @@ export default function reducer(state, action) {
 					id: Date.now(),
 				},
 			];
+		case 'REMOVE':
+			return state.filter(todo => todo.id !== action.payload);
+
+		case 'TOGGLE':
+			return state.map(todo => {
+				if (todo.id === action.payload) {
+					todo.completed = !todo.completed;
+				}
+				return todo;
+			});
 
 		default:
 			return state;
